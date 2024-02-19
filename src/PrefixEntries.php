ba7840b96aa9717d9840392292ad4593885b5e26
beedotnet <bryantwells@me.com>
2024-02-18 19:25:44 -0500

    schema check

--- src/PrefixEntries.php
+++ src/PrefixEntries.php
@@ -49,7 +49,7 @@ class PrefixEntries extends Plugin
     /**
      * @var string
      */
-    // public $schemaVersion = '1.0.0';
+    public string $schemaVersion = '1.0.0';
 
     /**
      * @var bool
